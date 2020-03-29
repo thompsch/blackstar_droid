86e5b891d2dd786e0cd78a20dcf7fc15e94e0cc7
MongoCaleb <caleb.thompson@mongodb.com>
2020-03-28 21:35:07 -0700

    controls wired up. Need to add images

--- app/src/main/java/com/ca13b/blackdroid/ui/ControlsFragment.java
+++ app/src/main/java/com/ca13b/blackdroid/ui/ControlsFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,8 +23,18 @@ public class ControlsFragment extends Fragment {
     private ControlsViewModel controlsViewModel;
     private SeekBar sbVolume;
     private SeekBar sbGain;
+    private SeekBar sbBass;
+    private SeekBar sbMid;
+    private SeekBar sbTreble;
+    private Spinner voices;
+    private SeekBar sbIsf;
     Control ctrlGain;
     Control ctrlVolume;
+    Control ctrlBass;
+    Control ctrlMid;
+    Control ctrlTreble;
+    Control ctrlVoice;
+    Control ctrlIsf;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,8 +44,14 @@ public class ControlsFragment extends Fragment {
         initializeControls(root);
 
         amp = ((MainActivity)getActivity()).blackstarAmp;
+        ctrlVoice = amp.Controls.get(1);
         ctrlGain = amp.Controls.get(2);
         ctrlVolume = amp.Controls.get(3);
+        ctrlBass = amp.Controls.get(4);
+        ctrlMid = amp.Controls.get(5);
+        ctrlTreble = amp.Controls.get(6);
+        ctrlIsf = amp.Controls.get(7);
+
 
         //TODO: get current settings and set sliders accordingly!
 
@@ -42,32 +61,74 @@ public class ControlsFragment extends Fragment {
 
     private void initializeControls(final View root) {
         sbGain = root.findViewById(R.id.gain_slider);
-        sbGain.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress = 0;
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) { progress = progresValue;}
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { amp.SetControlValue(ctrlGain, (progress*ctrlGain.maxValue)/100); }
-
-        });
-
+        sbGain.setOnSeekBarChangeListener(seekBarChanged);
         sbVolume = root.findViewById(R.id.volume_slider);
-        sbVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress = 0;
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) { progress = progresValue;}
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { amp.SetControlValue(ctrlVolume, (progress*ctrlGain.maxValue)/100); }
-        });
+        sbVolume.setOnSeekBarChangeListener(seekBarChanged);
+        sbBass = root.findViewById(R.id.bass_slider);
+        sbBass.setOnSeekBarChangeListener(seekBarChanged);
+        sbMid = root.findViewById(R.id.middle_slider);
+        sbMid.setOnSeekBarChangeListener(seekBarChanged);
+        sbTreble = root.findViewById(R.id.treble_slider);
+        sbTreble.setOnSeekBarChangeListener(seekBarChanged);
+        voices = root.findViewById(R.id.voice_list);
+        voices.setOnItemSelectedListener(ddChange);
+        sbIsf = root.findViewById(R.id.isf_slider);
+        sbIsf.setOnSeekBarChangeListener(seekBarChanged);
     }
+
+    private AdapterView.OnItemSelectedListener ddChange = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            amp.SetControlValue(ctrlVoice, position);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {}
+    };
+
+    private SeekBar.OnSeekBarChangeListener seekBarChanged = new SeekBar.OnSeekBarChangeListener() {
+        int progress = 0;
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) { progress = progresValue;}
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) { }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            Control ctrlTemp = null;
+
+            switch (seekBar.getId()){
+                case R.id.gain_slider: {
+                    ctrlTemp = ctrlGain;
+                    break;
+                }
+                case R.id.volume_slider: {
+                    ctrlTemp = ctrlVolume;
+                    break;
+                }
+                case R.id.bass_slider: {
+                    ctrlTemp = ctrlBass;
+                    break;
+                }
+                case R.id.middle_slider: {
+                    ctrlTemp = ctrlMid;
+                    break;
+                }
+                case R.id.treble_slider: {
+                    ctrlTemp = ctrlTreble;
+                    break;
+                }
+                case R.id.isf_slider: {
+                    ctrlTemp = ctrlIsf;
+                    break;
+                }
+            }
+            if (ctrlTemp == null) return;
+            amp.SetControlValue(ctrlTemp, (progress*ctrlTemp.maxValue)/100);
+            Toast.makeText(getContext(), String.format("I set %s to %d", ctrlTemp.controlName, progress), Toast.LENGTH_LONG).show();
+
+        }
+    };
 }
